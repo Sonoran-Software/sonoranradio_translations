36c03c1257749a64aa9f4fce50d0fa1848df311c
Brian Sosnowski <brian@sonoransoftware.com>
2026-07-17 08:40:38 -0700

    Fix language selector metadata

--- index.ts
+++ index.ts
@@ -2,52 +2,61 @@ import ar from './ar.json';
 import cs from './cs.json';
 import da from './da.json';
 import de from './de.json';
-import en from './en.json';
 import enUS from './en-US.json';
-import es from './es.json';
 import esEs from './es-ES.json';
 import fi from './fi.json';
 import fr from './fr.json';
 import it from './it.json';
 import ja from './ja.json';
 import ko from './ko.json';
-import koKR from './ko-KR.json';
 import nl from './nl.json';
-import nlBE from './nl-BE.json';
 import pl from './pl.json';
 import pt from './pt.json';
 import ru from './ru.json';
 import sv from './sv.json';
 import tr from './tr.json';
-import uk from './uk.json';
 import zh from './zh.json';
-import zhCN from './zh-CN.json';
-import zhTW from './zh-TW.json';
 
-export default {
+const messages = {
   ar: ar, // Arabic
   cs: cs, // Czech
   da: da, // Danish
   de: de, // German
-  en: en, // English
   'en-US': enUS, // English
-  es: es, // Spanish
   'es-ES': esEs, // Spanish
   fi: fi, // Finnish
   fr: fr, // French
   it: it, // Italian
   ja: ja, // Japanese
   ko: ko, // Korean
-  'ko-KR': koKR, // Korean
   nl: nl, // Dutch
-  'nl-BE': nlBE, // Dutch (Belgium)
   pl: pl, // Polish
   pt: pt, // Portuguese
   ru: ru, // Russian
   sv: sv, // Swedish
   tr: tr, // Turkish
-  uk: uk, // Ukrainian
   zh: zh, // Chinese
-  'zh-CN': zhCN, // Chinese (Simplified)
-  'zh-TW': zhTW, // Chinese (Traditional)
 };
+
+export const languageNames: Record<keyof typeof messages, string> = {
+  ar: 'العربية',
+  cs: 'Čeština',
+  da: 'Dansk',
+  de: 'Deutsch',
+  'en-US': 'English (US)',
+  'es-ES': 'Español (España)',
+  fi: 'Suomi',
+  fr: 'Français',
+  it: 'Italiano',
+  ja: '日本語',
+  ko: '한국어',
+  nl: 'Nederlands',
+  pl: 'Polski',
+  pt: 'Português',
+  ru: 'Русский',
+  sv: 'Svenska',
+  tr: 'Türkçe',
+  zh: '中文',
+};
+
+export default messages;
